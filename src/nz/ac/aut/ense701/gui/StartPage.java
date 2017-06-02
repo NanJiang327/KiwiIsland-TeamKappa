33d988100934ac8c7dc54f1378bb5b2be1fb2764
Nan <nj499521010@gmail.com>
2017-06-02 15:30:32 +1200

    Remove character selection since there is no value according to the feedback

--- src/nz/ac/aut/ense701/gui/StartPage.java
+++ src/nz/ac/aut/ense701/gui/StartPage.java
@@ -18,9 +18,9 @@ public class StartPage {
 	 private JFrame startFrame;
 	 private BackPanel startPanel;
 	 private JButton JbStart,JbCancel;
-	 private JRadioButton JRBSmall,JRBNormall,JRBBig,JRBCdv,JRBCmario,JRBCbm;
-	 private ButtonGroup bsGroup,charGroup;
-	 private JLabel boardsizeLabel,charcterLabel;
+	 private JRadioButton JRBSmall,JRBNormall,JRBBig;
+	 private ButtonGroup bsGroup;
+	 private JLabel boardsizeLabel;
 	 private String gameBgm = "1";
      private String gameCharacter = "warrior.png"; //defaults to warrior.png
      private int gameBoard = 10;
@@ -39,14 +39,10 @@ public class StartPage {
 	     ImageIcon CancelIcon = new ImageIcon(getClass().getResource("/images/icon/Cancel.png"));
 	     
 	     //set labels
-	     boardsizeLabel =new JLabel("Choose board size.(TODO for next sprint)",JLabel.CENTER);
+	     boardsizeLabel =new JLabel("Choose board size",JLabel.CENTER);
 	     boardsizeLabel.setFont(new Font("Dialog",1,20));
-	     boardsizeLabel.setBounds(0,0,768,25);
+	     boardsizeLabel.setBounds(0,0,768,220);
 	     
-	     charcterLabel =new JLabel("Choose your character.",JLabel.CENTER);
-	     charcterLabel.setFont(new Font("Dialog",1,20));
-	     charcterLabel.setBounds(0,180,768,25);
-
 	     //Create start button       
 	     JbStart = new JButton();
 	     JbStart.setIcon(startIcon);
@@ -92,7 +88,7 @@ public class StartPage {
 	      
 	      //set board size buttons
 	      JRBSmall = new JRadioButton("Small (8x8)");
-	      JRBSmall.setBounds(349,45,150,40);
+	      JRBSmall.setBounds(349,140,150,40);
 	      JRBSmall.setOpaque(false);
 	      JRBSmall.addActionListener(new ActionListener(){
 			@Override
@@ -103,7 +99,7 @@ public class StartPage {
 	      });
 	      
 	      JRBNormall = new JRadioButton("Normall (10x10)");
-	      JRBNormall.setBounds(349,85,150,40);
+	      JRBNormall.setBounds(349,180,150,40);
 	      JRBNormall.setOpaque(false);
 	      JRBNormall.setSelected(true);
 	      JRBNormall.addActionListener(new ActionListener(){
@@ -115,7 +111,7 @@ public class StartPage {
 	      });
 	      
 	      JRBBig = new JRadioButton("Big (12x12)");
-	      JRBBig.setBounds(349,125,150,40);
+	      JRBBig.setBounds(349,220,150,40);
 	      JRBBig.setOpaque(false);
 	      JRBBig.addActionListener(new ActionListener(){
 			@Override
@@ -124,59 +120,17 @@ public class StartPage {
 			}	    	  
 	      });
 	      
-	      //set up character buttons
-	      JRBCdv = new JRadioButton("Warrior");
-	      JRBCdv.setSelected(true);
-	      JRBCdv.setBounds(349,200,100,50);
-	      JRBCdv.setOpaque(false);
-	      JRBCdv.addActionListener(new ActionListener(){
-			@Override
-			public void actionPerformed(ActionEvent arg0) {
-                            gameCharacter = "warrior.png";
-			}	    	  
-	      });
-	      
-	      JRBCmario = new JRadioButton("Archer");
-	      JRBCmario.setBounds(349,240,100,50);
-	      JRBCmario.setOpaque(false);
-	      JRBCmario.addActionListener(new ActionListener(){
-			@Override
-			public void actionPerformed(ActionEvent arg0) {
-                            gameCharacter = "archer.png";
-			}	    	  
-	      });
-	      
-	      JRBCbm = new JRadioButton("Wizard");
-	      JRBCbm.setBounds(349,280,100,50);
-	      JRBCbm.setOpaque(false);
-	      JRBCbm.addActionListener(new ActionListener(){
-			@Override
-			public void actionPerformed(ActionEvent arg0) {
-                            gameCharacter = "wizard.png";
-			}	    	  
-	      });
 	      
 	      bsGroup = new ButtonGroup();
 	      bsGroup.add(JRBSmall);
 	      bsGroup.add(JRBNormall);
 	      bsGroup.add(JRBBig);
 	      
-
-	      
-	      charGroup = new ButtonGroup();
-	      charGroup.add(JRBCdv);
-	      charGroup.add(JRBCmario);
-	      charGroup.add(JRBCbm);
-	      
 	     startPanel.setLayout(null);
 	     startPanel.add(boardsizeLabel);
 	     startPanel.add(JRBSmall);
 	     startPanel.add(JRBNormall);
 	     startPanel.add(JRBBig);
-	     startPanel.add(charcterLabel);
-	     startPanel.add(JRBCdv);
-	     startPanel.add(JRBCmario);
-	     startPanel.add(JRBCbm);
 	     startPanel.add(JbStart);
 	     startPanel.add(JbCancel);     
 	       
@@ -187,5 +141,9 @@ public class StartPage {
 	     startFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);  
 	     startFrame.setVisible(true);  
 		 
+	 }
+	 
+	 public static void main(String[] args){
+		 new StartPage("2");
 	 }
 }
